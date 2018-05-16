fc7436b25baf8a40f3241e59cd3423dbc33b4ff7
RajaVamsi Chidella <rajavamsi.chidella@gmail.com>
2018-05-16 17:54:02 -0500

    initial submit
    initial submit

---
+++ Counter Calorie/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Counter_Calorie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxBanana = new System.Windows.Forms.PictureBox();
+            this.picBoxPear = new System.Windows.Forms.PictureBox();
+            this.picBoxOrange = new System.Windows.Forms.PictureBox();
+            this.picBoxApple = new System.Windows.Forms.PictureBox();
+            this.lblTotalCalories = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCtBan = new System.Windows.Forms.Label();
+            this.lblCtPea = new System.Windows.Forms.Label();
+            this.lblCtOra = new System.Windows.Forms.Label();
+            this.lblCtApp = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblTC = new System.Windows.Forms.Label();
+            this.btnResetBanana = new System.Windows.Forms.Button();
+            this.btnPearReset = new System.Windows.Forms.Button();
+            this.btnOrgReset = new System.Windows.Forms.Button();
+            this.btnAppleReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxBanana)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOrange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxApple)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxBanana
+            // 
+            this.picBoxBanana.Image = global::Counter_Calorie.Properties.Resources.BananaCalories;
+            this.picBoxBanana.Location = new System.Drawing.Point(113, 56);
+            this.picBoxBanana.Name = "picBoxBanana";
+            this.picBoxBanana.Size = new System.Drawing.Size(100, 98);
+            this.picBoxBanana.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxBanana.TabIndex = 0;
+            this.picBoxBanana.TabStop = false;
+            this.picBoxBanana.Click += new System.EventHandler(this.picBoxBanana_Click);
+            // 
+            // picBoxPear
+            // 
+            this.picBoxPear.Image = global::Counter_Calorie.Properties.Resources.PearCalories;
+            this.picBoxPear.Location = new System.Drawing.Point(328, 56);
+            this.picBoxPear.Name = "picBoxPear";
+            this.picBoxPear.Size = new System.Drawing.Size(100, 98);
+            this.picBoxPear.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxPear.TabIndex = 1;
+            this.picBoxPear.TabStop = false;
+            this.picBoxPear.Click += new System.EventHandler(this.picBoxPear_Click);
+            // 
+            // picBoxOrange
+            // 
+            this.picBoxOrange.Image = global::Counter_Calorie.Properties.Resources.OrangeCalories;
+            this.picBoxOrange.Location = new System.Drawing.Point(113, 297);
+            this.picBoxOrange.Name = "picBoxOrange";
+            this.picBoxOrange.Size = new System.Drawing.Size(100, 100);
+            this.picBoxOrange.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxOrange.TabIndex = 2;
+            this.picBoxOrange.TabStop = false;
+            this.picBoxOrange.Click += new System.EventHandler(this.picBoxOrange_Click);
+            // 
+            // picBoxApple
+            // 
+            this.picBoxApple.Image = global::Counter_Calorie.Properties.Resources.AppleCalories;
+            this.picBoxApple.Location = new System.Drawing.Point(328, 297);
+            this.picBoxApple.Name = "picBoxApple";
+            this.picBoxApple.Size = new System.Drawing.Size(100, 100);
+            this.picBoxApple.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxApple.TabIndex = 3;
+            this.picBoxApple.TabStop = false;
+            this.picBoxApple.Click += new System.EventHandler(this.picBoxApple_Click);
+            // 
+            // lblTotalCalories
+            // 
+            this.lblTotalCalories.AutoSize = true;
+            this.lblTotalCalories.Location = new System.Drawing.Point(578, 121);
+            this.lblTotalCalories.Name = "lblTotalCalories";
+            this.lblTotalCalories.Size = new System.Drawing.Size(51, 17);
+            this.lblTotalCalories.TabIndex = 8;
+            this.lblTotalCalories.Text = "ResTC";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(562, 313);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(562, 370);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblCtBan
+            // 
+            this.lblCtBan.AutoSize = true;
+            this.lblCtBan.Location = new System.Drawing.Point(138, 204);
+            this.lblCtBan.Name = "lblCtBan";
+            this.lblCtBan.Size = new System.Drawing.Size(67, 17);
+            this.lblCtBan.TabIndex = 12;
+            this.lblCtBan.Text = "ResCBan";
+            // 
+            // lblCtPea
+            // 
+            this.lblCtPea.AutoSize = true;
+            this.lblCtPea.Location = new System.Drawing.Point(348, 204);
+            this.lblCtPea.Name = "lblCtPea";
+            this.lblCtPea.Size = new System.Drawing.Size(67, 17);
+            this.lblCtPea.TabIndex = 13;
+            this.lblCtPea.Text = "ResCPea";
+            // 
+            // lblCtOra
+            // 
+            this.lblCtOra.AutoSize = true;
+            this.lblCtOra.Location = new System.Drawing.Point(138, 455);
+            this.lblCtOra.Name = "lblCtOra";
+            this.lblCtOra.Size = new System.Drawing.Size(66, 17);
+            this.lblCtOra.TabIndex = 14;
+            this.lblCtOra.Text = "ResCOra";
+            // 
+            // lblCtApp
+            // 
+            this.lblCtApp.AutoSize = true;
+            this.lblCtApp.Location = new System.Drawing.Point(348, 455);
+            this.lblCtApp.Name = "lblCtApp";
+            this.lblCtApp.Size = new System.Drawing.Size(67, 17);
+            this.lblCtApp.TabIndex = 15;
+            this.lblCtApp.Text = "ResCApp";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(110, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Count of Banana";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(333, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Count of Pear";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(111, 423);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Count of Orange";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(327, 423);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Count of Apple";
+            // 
+            // lblTC
+            // 
+            this.lblTC.AutoSize = true;
+            this.lblTC.Location = new System.Drawing.Point(559, 74);
+            this.lblTC.Name = "lblTC";
+            this.lblTC.Size = new System.Drawing.Size(95, 17);
+            this.lblTC.TabIndex = 20;
+            this.lblTC.Text = "Total Calories";
+            // 
+            // btnResetBanana
+            // 
+            this.btnResetBanana.Location = new System.Drawing.Point(113, 233);
+            this.btnResetBanana.Name = "btnResetBanana";
+            this.btnResetBanana.Size = new System.Drawing.Size(111, 46);
+            this.btnResetBanana.TabIndex = 21;
+            this.btnResetBanana.Text = "Banana Reset";
+            this.btnResetBanana.UseVisualStyleBackColor = true;
+            this.btnResetBanana.Click += new System.EventHandler(this.btnResetBanana_Click);
+            // 
+            // btnPearReset
+            // 
+            this.btnPearReset.Location = new System.Drawing.Point(330, 233);
+            this.btnPearReset.Name = "btnPearReset";
+            this.btnPearReset.Size = new System.Drawing.Size(98, 46);
+            this.btnPearReset.TabIndex = 22;
+            this.btnPearReset.Text = "Pear Reset";
+            this.btnPearReset.UseVisualStyleBackColor = true;
+            this.btnPearReset.Click += new System.EventHandler(this.btnPearReset_Click);
+            // 
+            // btnOrgReset
+            // 
+            this.btnOrgReset.Location = new System.Drawing.Point(113, 483);
+            this.btnOrgReset.Name = "btnOrgReset";
+            this.btnOrgReset.Size = new System.Drawing.Size(111, 45);
+            this.btnOrgReset.TabIndex = 23;
+            this.btnOrgReset.Text = "Orange Reset";
+            this.btnOrgReset.UseVisualStyleBackColor = true;
+            this.btnOrgReset.Click += new System.EventHandler(this.btnOrgReset_Click);
+            // 
+            // btnAppleReset
+            // 
+            this.btnAppleReset.Location = new System.Drawing.Point(322, 488);
+            this.btnAppleReset.Name = "btnAppleReset";
+            this.btnAppleReset.Size = new System.Drawing.Size(106, 40);
+            this.btnAppleReset.TabIndex = 24;
+            this.btnAppleReset.Text = "Apple Reset";
+            this.btnAppleReset.UseVisualStyleBackColor = true;
+            this.btnAppleReset.Click += new System.EventHandler(this.btnAppleReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 645);
+            this.Controls.Add(this.btnAppleReset);
+            this.Controls.Add(this.btnOrgReset);
+            this.Controls.Add(this.btnPearReset);
+            this.Controls.Add(this.btnResetBanana);
+            this.Controls.Add(this.lblTC);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblCtApp);
+            this.Controls.Add(this.lblCtOra);
+            this.Controls.Add(this.lblCtPea);
+            this.Controls.Add(this.lblCtBan);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblTotalCalories);
+            this.Controls.Add(this.picBoxApple);
+            this.Controls.Add(this.picBoxOrange);
+            this.Controls.Add(this.picBoxPear);
+            this.Controls.Add(this.picBoxBanana);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Counter Calorie";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxBanana)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOrange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxApple)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxBanana;
+        private System.Windows.Forms.PictureBox picBoxPear;
+        private System.Windows.Forms.PictureBox picBoxOrange;
+        private System.Windows.Forms.PictureBox picBoxApple;
+        private System.Windows.Forms.Label lblTotalCalories;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCtBan;
+        private System.Windows.Forms.Label lblCtPea;
+        private System.Windows.Forms.Label lblCtOra;
+        private System.Windows.Forms.Label lblCtApp;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblTC;
+        private System.Windows.Forms.Button btnResetBanana;
+        private System.Windows.Forms.Button btnPearReset;
+        private System.Windows.Forms.Button btnOrgReset;
+        private System.Windows.Forms.Button btnAppleReset;
+    }
+}
+
---
+++ Counter Calorie/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Counter_Calorie
+{
+    public partial class Form1 : Form
+    {
+        int totalCalories=0;
+        int BanCt = 0;
+        int PeaCt = 0;
+        int AppCt = 0;
+        int OraCt = 0;
+        const int BAN_CAL = 115;
+        const int PEA_CAL = 120;
+        const int ORA_CAL = 90;
+        const int APP_CAL = 80;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picBoxBanana_Click(object sender, EventArgs e)
+        {
+            
+            BanCt = BanCt + 1;
+            totalCalories = totalCalories + (BanCt * BAN_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            lblCtBan.Text = BanCt.ToString();
+                       
+        }
+
+        private void picBoxPear_Click(object sender, EventArgs e)
+        {
+           
+            PeaCt = PeaCt + 1;
+            totalCalories = totalCalories + (PeaCt * PEA_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            lblCtPea.Text = PeaCt.ToString();
+        }
+
+        private void picBoxOrange_Click(object sender, EventArgs e)
+        {
+           
+            OraCt = OraCt + 1;
+            totalCalories = totalCalories + (OraCt * ORA_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            lblCtOra.Text = OraCt.ToString();
+        }
+
+        private void picBoxApple_Click(object sender, EventArgs e)
+        {
+            
+            AppCt = AppCt + 1;
+            totalCalories = totalCalories + (AppCt * APP_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            lblCtApp.Text = AppCt.ToString();
+        }
+
+        private void btnResetBanana_Click(object sender, EventArgs e)
+        {
+            totalCalories = totalCalories - (BanCt * BAN_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            BanCt = 0;
+            lblCtBan.Text = 0.ToString();
+
+        }
+
+        private void btnPearReset_Click(object sender, EventArgs e)
+        {
+            totalCalories = totalCalories - (PeaCt * PEA_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            PeaCt = 0;
+            lblCtPea.Text = 0.ToString();
+
+        }
+
+        private void btnOrgReset_Click(object sender, EventArgs e)
+        {
+           totalCalories = totalCalories - (OraCt * ORA_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            OraCt = 0;
+            lblCtOra.Text = 0.ToString();
+        }
+
+        private void btnAppleReset_Click(object sender, EventArgs e)
+        {
+            totalCalories = totalCalories - (AppCt * APP_CAL);
+            lblTotalCalories.Text = totalCalories.ToString();
+            AppCt = 0;
+            lblCtApp.Text = 0.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblTotalCalories.Text = 0.ToString();
+            lblCtBan.Text = 0.ToString();
+            lblCtPea.Text = 0.ToString();
+            lblCtOra.Text = 0.ToString();
+            lblCtApp.Text = 0.ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
